afe0544a95e672afbe4de8c89277e15c30ae27a1
samiylo <skryshu@gmail.com>
2025-04-26 15:56:25 -0500

    Setup Proxy Request to Service

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [message, setMessage] = useState('');
+
+  const fetchMessage = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/ask/nesha');
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.text();
+      setMessage(data);
+    } catch (error) {
+      console.error('Error fetching message:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Good morning Nesha
-        </p>
+        <p>Good morning Nesha</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
+        <button  className="btn btn-primary" onClick={fetchMessage}>Ask Nesha</button>
+        {message && <p>{message}</p>}
       </header>
     </div>
   );
